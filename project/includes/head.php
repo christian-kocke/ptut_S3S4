86a08f1a21457d84a4d1a7ebdb0ec14d6303f0fe
Yann VERNEAU <Yann@Yanns-MacBook-Pro.local>
2015-02-12 18:33:00 +0100

    ajout footer

--- project/includes/head.php
+++ project/includes/head.php
@@ -5,7 +5,6 @@
 	<meta name = "viewport" content = "width = device-width, initial-scale = 0.4, user-scalable = yes">
 	
 
-	<link rel="stylesheet" href="css/style.css">
 
 	<link rel="stylesheet" href="css/foundation-datepicker.css">
 	<!-- include foundation css -->
