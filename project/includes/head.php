d6849eacf4a899d765d2a1723e18a6d3361586cf
christian-kocke <christian.kocke@gmail.com>
2015-02-12 18:35:49 +0100

    modif header

--- project/includes/head.php
+++ project/includes/head.php
@@ -123,24 +123,24 @@
 			</section>
 		</nav>
 		<div class="off-canvas-wrap" data-offcanvas>
-	  	<div class="inner-wrap">
-	    	<nav class="tab-bar">
-		      	<section class="left tab-bar-section">
-		        	<h1 class="title">Le restaurant</h1>
-		      	</section>
+		  	<div class="inner-wrap">
+		    	<nav class="tab-bar">
+			      	<section class="left tab-bar-section">
+			        	<h1 class="title">Le restaurant</h1>
+			      	</section>
 
-		      	<section class="right-small">
-		        	<a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
-		      	</section>
-		    </nav>		    
-		    <aside class="right-off-canvas-menu">
-		      <ul class="off-canvas-list">
-		        <li><label>Foundation</label></li>
-		        <li><a href="#">The Traders</a></li>
-		        <li><a href="#">The Merchant Princes</a></li>
-		      </ul>
-		    </aside>
-		<a class="exit-off-canvas"></a>
-	  </div>
-	</div>
+			      	<section class="right-small">
+			        	<a class="right-off-canvas-toggle menu-icon" href="#"><span></span></a>
+			      	</section>
+			    </nav>		    
+			    <aside class="right-off-canvas-menu">
+			      <ul class="off-canvas-list">
+			        <li><label>Foundation</label></li>
+			        <li><a href="#">The Traders</a></li>
+			        <li><a href="#">The Merchant Princes</a></li>
+			      </ul>
+			    </aside>
+				<a class="exit-off-canvas"></a>
+		  	</div>
+		</div>
 	</header>
