c7b5c4d7b282b58ea09dc8c7bb4fa49f59f86c6c
christian-kocke <christian.kocke@gmail.com>
2015-02-12 17:32:08 +0100

    add smooth scroll

--- project/includes/footer.php
+++ project/includes/footer.php
@@ -1,6 +1,7 @@
 	<footer>
 		<div>©Le Restaurant 2014</div>
 		<div>Back to <a href="#top">top</div>
+		<script src="js/index.js"></script>
 		<script src="js/vendor/fastclick.js"></script>
 		<script src="js/foundation.min.js"></script>
 		<script>
---
+++ project/includes/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ // smooth scroll function
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+		
+		var target = this.hash,
+		$target = $(target);
+		
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+			}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
+	});					
+});  
