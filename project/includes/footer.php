730e93c4d94c8fcf44e6dc3c80cc84696243de9b
VIRICEL <noe.viricel@orange.fr>
2015-03-02 10:07:25 +0100

    footer màj

--- project/includes/footer.php
+++ project/includes/footer.php
@@ -1,6 +1,51 @@
 		<footer>
 			<!-- CREDITS -->
 			<div class="row">
+
+				<!-- SOCIAL NETWORKS -->
+				<div class="small-4 columns">
+					<p class="text-center"> Suivez-nous sur les réseaux sociaux ! </p>
+					<div class="row">
+
+						<!-- FACEBOOK -->
+						<div class="small-3 columns">
+							<a href=""><span class="fa-stack fa-lg">
+								<i class="fa fa-square-o fa-stack-2x"></i>
+								<i class="fa fa-facebook fa-stack-1x"></i>
+							</span></a>
+						</div><!-- class small-3 columns -->
+
+						<!-- TWITTER -->
+						<div class="small-3 columns">
+							<a href=""><span class="fa-stack fa-lg">
+								<i class="fa fa-square-o fa-stack-2x"></i>
+								<i class="fa fa-twitter fa-stack-1x"></i>
+							</span></a>
+						</div><!-- class small-3 columns -->
+
+						<!-- GOOGLE PLUS -->
+						<div class="small-3 columns">
+							<a href=""><span class="fa-stack fa-lg">
+								<i class="fa fa-square-o fa-stack-2x"></i>
+								<i class="fa fa-google-plus fa-stack-1x"></i>
+							</span></a>
+						</div><!-- class small-3 columns -->
+
+						<!-- LINKEDIN -->
+						<div class="small-3 columns">
+							<a href=""><span class="fa-stack fa-lg">
+								<i class="fa fa-square-o fa-stack-2x"></i>
+								<i class="fa fa-linkedin fa-stack-1x"></i>
+							</span></a>
+						</div><!-- class small-3 columns -->
+
+					</div><!-- class row -->
+				</div><!-- class small-4 columns -->
+
+				<div class="small-8 columns flex-video">
+					<iframe src="https://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=fr&amp;geocode=&amp;q=1+Rue+de+la+Technologie,+Villeurbanne&amp;aq=0&amp;oq=1+rue+de+la+techno&amp;sll=45.771758,4.889826&amp;sspn=0.085253,0.181789&amp;ie=UTF8&amp;hq=&amp;hnear=1+Rue+de+la+Technologie,+69100+Villeurbanne,+Rh%C3%B4ne,+Rh%C3%B4ne-Alpes&amp;t=m&amp;ll=45.785901,4.882221&amp;spn=0.02873,0.054932&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
+				</div><!-- class small-8 -->
+				
 				<div class="small-12 columns">
 					<p class="text-center"> ©Le Restaurant 2014 </p>
 				</div><!-- class small-12 columns -->
